9f15d5a4283e95a771bf7938832dd36001e41302
Ali Kemal Tanriverdi <11221504+alikemaltanriverdi@users.noreply.github.com>
2024-07-23 14:59:59 -0400

    created models for the rest of the tables

---
+++ app/Http/Controllers/ActivityValueController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreActivityValueRequest;
+use App\Http\Requests\UpdateActivityValueRequest;
+use App\Models\ActivityValue;
+
+class ActivityValueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreActivityValueRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ActivityValue $activityValue)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ActivityValue $activityValue)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateActivityValueRequest $request, ActivityValue $activityValue)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ActivityValue $activityValue)
+    {
+        //
+    }
+}
