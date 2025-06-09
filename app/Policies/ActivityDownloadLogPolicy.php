8c341960c408f8d79fff9e1992079c6ae10b6050
Ali Kemal Tanriverdi <11221504+alikemaltanriverdi@users.noreply.github.com>
2025-06-09 18:36:35 -0400

    Policy enforcements, and file download bug fix

--- app/Policies/ActivityDownloadLogPolicy.php
+++ app/Policies/ActivityDownloadLogPolicy.php
@@ -2,26 +2,31 @@
 
 namespace App\Policies;
 
-use App\Models\ActivityLog;
+use App\Models\ActivityDownloadLog;
+use App\Models\Permission;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class ActivityLogPolicy
+class ActivityDownloadLogPolicy
 {
     /**
      * Determine whether the user can view any models.
      */
     public function viewAny(User $user): bool
     {
-        //
+        return Permission::where('user_id',$user->id)
+            ->where('permission','manage_activities')
+            ->exists();
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, ActivityLog $activityLog): bool
+    public function view(User $user): bool
     {
-        //
+        return Permission::where('user_id',$user->id)
+            ->where('permission','manage_activities')
+            ->exists();
     }
 
     /**
@@ -29,38 +34,38 @@ class ActivityLogPolicy
      */
     public function create(User $user): bool
     {
-        //
+        return false;
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, ActivityLog $activityLog): bool
+    public function update(User $user): bool
     {
-        //
+        return false;
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, ActivityLog $activityLog): bool
+    public function delete(User $user): bool
     {
-        //
+        return false;
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, ActivityLog $activityLog): bool
+    public function restore(User $user): bool
     {
-        //
+        return false;
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, ActivityLog $activityLog): bool
+    public function forceDelete(User $user, ActivityDownloadLog $ActivityDownloadLog): bool
     {
-        //
+        return false;
     }
 }
