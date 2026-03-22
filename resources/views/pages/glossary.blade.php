e00bdb8c50ffe041c6da92452b158d5e964ce54f
Efe Cankat Turkmen <44501801+efecankatturkmen@users.noreply.github.com>
2026-03-22 19:26:34 -0400

    Update help text styling
    Changed the background color and text color of the help text in the glossary page to enhance readability and ensure better contrast for users.

--- resources/views/pages/glossary.blade.php
+++ resources/views/pages/glossary.blade.php
@@ -16,7 +16,7 @@
             <div class="panel-body" style="font-size: 20px;">
                 <h2>{{$type->type}}</h2>
                 @if (isset($type->help_text))
-                    <p class="label label-default" style="background-color:#009ee0; display:inline-block;">{{$type->help_text}}</p><br>
+                    <p class="label label-default" style="background-color:#006fa3; color:#fff; display:inline-block;">{{$type->help_text}}</p><br>
                 @endif
                 <dl>
                 @foreach($type['values'] as $value)
